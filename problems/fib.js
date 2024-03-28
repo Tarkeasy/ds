cebf27880bc47966e2516cb58f3d828abb833032
Taras Kulchytskyi <kulchytskyitaras@gmail.com>
2024-03-28 10:24:14 +0200

    feat: add merge sort

--- problems/fib.js
+++ problems/fib.js
@@ -1,31 +1,37 @@
 
 // Time complexity O(n)
-// const fibo = (n) => {
+const fibo = (n) => {
 
-// 	const fibNumbers = [0, 1];
+	const fibNumbers = [0, 1];
 
-// 	for (let i = 2; i < n; i++) {
-// 		fibNumbers[i] = (fibNumbers[i - 1] + fibNumbers[i - 2])
-// 	}
+	for (let i = 2; i < n; i++) {
+		fibNumbers[i] = (fibNumbers[i - 1] + fibNumbers[i - 2])
+	}
 
-// 	return fibNumbers
+	return fibNumbers
 
-// }
+}
 
-// console.log('fibo', fibo(7))
+console.log('fibo', fibo(7))
 
-
-// 4
+const memo = {}
 const fib = (n) => {
 
-	if(n < 2) return n;
-	//         4
+	if (n < 2) {
+		return n;
+	}
+	//         5
 	//        / \
 	//   		3    2
 	//  	 / \	/	\
 	// 		2	 1	1  0
 	// 	 /\
 	// 	 1 0
-		return fib(n - 1) + fib(n - 2);
+
+	if (!(n in memo)) {
+		memo[n] = fib(n - 1) + fib(n - 2);
+	}
+	return memo[n];
+
 }
-console.log(fib(4))
+console.log(fib(7))
--- problems/sort.js
+++ problems/sort.js
@@ -43,21 +43,57 @@
 
 // Worst O(n^2) if every iteration pivot is the smallest number of the array or the greatest number
 // Average O(n logn) every iteration pivot in the middle or not the greatest ot the smallest number
-function quickSort(arr) {
-	if (arr.length <= 1) {
+// function quickSort(arr) {
+// 	if (arr.length <= 1) {
+// 		return arr
+// 	}
+// 	let left = []
+// 	let pivot = arr[arr.length - 1]
+// 	let right = []
+// 	for (let i = 0; i < arr.length - 1; i++) {
+// 		if (arr[i] < pivot) {
+// 			left.push(arr[i])
+// 		} else {
+// 			right.push(arr[i])
+// 		}
+// 	}
+// 	return [...quickSort(left), pivot, ...quickSort(right)]
+// }
+
+// console.log(quickSort([-1, -6, 3, 1, 9, 20, 2]))
+
+// O(n logn), but space complexity is worse then quickSort because it creates copy of the n array
+// So if we have space limitation, better use quickSort, otherwise mergeSort is faster in worst case scenario
+function mergeSort(arr) {
+	if (arr.length < 2) {
 		return arr
 	}
-	let left = []
-	let pivot = arr[arr.length - 1]
-	let right = []
-	for (let i = 0; i < arr.length - 1; i++) {
-		if (arr[i] < pivot) {
-			left.push(arr[i])
-		} else {
-			right.push(arr[i])
+	let mid = Math.floor(arr.length / 2)
+	const leftItems = arr.slice(0, mid)
+	const rightItems = arr.slice(mid)
+
+	function merge(left, right) {
+		let resultArray = [], leftIndex = 0, rightIndex = 0;
+
+		// Concatenating the values into the resultArray in order
+		while (leftIndex < left.length && rightIndex < right.length) {
+			if (left[leftIndex] < right[rightIndex]) {
+				resultArray.push(left[leftIndex]);
+				leftIndex++; // move left array cursor
+			} else {
+				resultArray.push(right[rightIndex]);
+				rightIndex++; // move right array cursor
+			}
 		}
+
+		// Concatenate here because there will be one element remaining
+		// from either left OR the right
+		return resultArray
+			.concat(left.slice(leftIndex))
+			.concat(right.slice(rightIndex));
 	}
-	return [...quickSort(left), pivot, ...quickSort(right)]
+
+	return merge(mergeSort(leftItems), mergeSort(rightItems))
 }
 
-console.log(quickSort([-1, -6, 3, 1, 9, 20, 2]))
+console.log(mergeSort([-1, -6, 3, 1, 9, 20, 2]))
